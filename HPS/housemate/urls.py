e71bfeaf5a3767084bf5d6882b75e39cfecdcfd2
Aman <amanniyaz.waplog@hotmail.com>
2020-12-11 16:42:44 +0000

    urls added

--- HPS/housemate/urls.py
+++ HPS/housemate/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('admin_login', views.admin_log, name='admin_log'),
+    path('user_login', views.user_log, name='user_log'),
+    path('myboard', views.myboard, name='myboard'),
+    path('register', views.register, name='register'),
+    path('registerLandlord', views.registerLandlord, name='registerLandlord'),
+    path('profile_view', views.profile_view, name='profile_view'),
 ]
